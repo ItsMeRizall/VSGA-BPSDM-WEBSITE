659afccfa4fbf0d177b8bfff04bd4a4f5df35217
ItsMeRizall <yoppy.sr2@gmail.com>
2023-09-01 08:45:47 +0700

    fix delete image from folder

--- services/delete_data.php
+++ services/delete_data.php
@@ -7,15 +7,20 @@ $dbHelper = new DBHelper("localhost", "root", "", "123_syahmi");
 
 if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) {
     $idToDelete = $_GET['id'];
-
+    $data = $dbHelper->getData("news", "images", "news_id = $idToDelete");
+    
     if ($dbHelper->deleteData("news", "news_id = $idToDelete")) {
-        $response = ['success' => true];
+        if(file_exists("../images/". $data[0]["images"])){
+            unlink("../images/". $data[0]["images"]);
+            echo "sukses hapus image";
+        }else{
+            echo "datanya ga ada";
+        }
+        header("Location: ../admin/admin.php");
+        exit();
     } else {
-        $response = ['success' => false];
+        echo "gagal menghapus";
     }
-
-    header('Content-Type: application/json');
-    echo json_encode($response);
 }
 
 ?>
